baa518f387a7cefb64ff85d20659be013c07d98a
Pantilei IAnulo <ianulov.pantilei@gmail.com>
2019-12-22 19:24:33 +0000

    Build

---
+++ client/build/precache-manifest.b85eaf195782eb1a5ed1bbbc21fcedc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c77c9c2226360597e65fec09d2ac0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd9ce810fb5420931d99",
+    "url": "/static/css/main.d894d8dc.chunk.css"
+  },
+  {
+    "revision": "0a8571c2c0d7df9ed733",
+    "url": "/static/js/2.fde8dc9e.chunk.js"
+  },
+  {
+    "revision": "fd9ce810fb5420931d99",
+    "url": "/static/js/main.c94368dd.chunk.js"
+  },
+  {
+    "revision": "4c40b94f0748754179e8",
+    "url": "/static/js/runtime-main.bbc06ef5.js"
+  },
+  {
+    "revision": "131eee19c7301c8e594ac6ebab506d93",
+    "url": "/static/media/Background.131eee19.png"
+  },
+  {
+    "revision": "e71a3a20f6416deef004609050e4d09c",
+    "url": "/static/media/home.e71a3a20.PNG"
+  }
+]);
